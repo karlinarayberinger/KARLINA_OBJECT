89bbf7fb71f003599f3cc863ed2c924c74cff1ca
karbytes <karlina.ray.beringer@protonmail.com>
2024-12-29 02:37:27 -0800

    Update pi_approximation.js

--- pi_approximation.js
+++ pi_approximation.js
@@ -536,12 +536,12 @@ function plot_random_pixel_on_square_canvas(simulation) {
  * Use the setInterval function to space plot_random_pixel_on_square_canvas(simulation) function calls apart by one second.
  */
 function start_monte_carlo_simulation() {
-    const time_point = Date.now(), p0 = '', p1 = '';
-    let simulation; // timer interval handler
-    const message = "The Monte Carlo simulation started at time: " + time_point + " milliseconds since 01_JANUARY_1970 00:00:00 (Coordinated Universal Time (UTC)).";
-    console.log(message);
-    document.getElementById("timestamped_events_log").innerHTML += p0 + message + p1;
-    initialize_application();
-    hide_start_button();
-    simulation = setInterval( function() { plot_random_pixel_on_square_canvas(simulation); }, 1000); // The plot_random_pixel_on_square_canvas(simulation) function is called once every 1000 milliseconds until the timer interval is cleared.
+	const time_point = Date.now(), p0 = '', p1 = '';
+	let simulation; // timer interval handler
+	const message = "The Monte Carlo simulation started at time: " + time_point + " milliseconds since 01_JANUARY_1970 00:00:00 (Coordinated Universal Time (UTC)).";
+	console.log(message);
+	document.getElementById("timestamped_events_log").innerHTML += p0 + message + p1;
+	initialize_application();
+	hide_start_button();
+	simulation = setInterval( function() { plot_random_pixel_on_square_canvas(simulation); }, 1000); // The plot_random_pixel_on_square_canvas(simulation) function is called once every 1000 milliseconds until the timer interval is cleared.
 }
